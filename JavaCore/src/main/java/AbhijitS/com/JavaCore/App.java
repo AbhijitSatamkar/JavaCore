624e6da4f4aa1f32d745870b66724832427e5271
Snehal Satamkar <snehabhi@Snehals-MacBook-Air.local>
2017-11-19 19:20:02 +0100

    create folder structure

---
+++ JavaCore/src/main/java/AbhijitS/com/JavaCore/App.java
@@ -0,0 +1,13 @@
+package AbhijitS.com.JavaCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
